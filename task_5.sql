87e6811ce5684d5f445bb401db9cef5b014ce790
morest1124 <morestburawa0-5@gmail.com>
2025-08-20 10:30:41 +0200

    correct spelling from Customers to customers

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
-INSECT INTO Customers (customer_id, customer_name, email, address)
+INSECT INTO customers (customer_id, customer_name, email, address)
 VALUE(1, Cole Baidoo, cbaidoo@sandtech.com, 123 Happiness Ave.)
